193631a6fdd7f04de0a10875387a26590b4db799
S.Hirudhanya <hirudhanya985@gmail.com>
2026-02-11 12:06:38 +0530

    Add files via upload

---
+++ java/CharaterToString.java
@@ -0,0 +1,11 @@
+package pst;
+
+public class CharaterToString {
+	public static void main (String[] args) {
+		char mychar ='H';
+		char[] a =new char[] {mychar};
+		String c = new String(a);
+		
+		System.out.println("string result:"+c);
+	}
+}
